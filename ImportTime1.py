1f424d554a78c2888e1a743be183581af96ef919
muamarzidan <89593258+muamarzidan@users.noreply.github.com>
2021-09-11 18:41:27 +0700

    Add files via upload

---
+++ ImportTime1.py
@@ -0,0 +1,90 @@
+import time
+
+print('\n')
+love = ['Muamar', 'Zidan', '2021']
+print(' '.join(love))
+print('\n')
+
+print('Wait for loading')
+print('\n')
+time.sleep(2)
+print('Muziro...')
+sentence = "Incoding"
+for char in sentence.split():
+    allChar = []
+    for y in range(12, -12, -1):
+        lst = []
+        lst_con = ''
+        for x in range(-30, 30):
+            formula = ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3
+            if formula <= 0:
+                lst_con += char[(x) % len(char)]
+            else:
+                lst_con += ' '
+        lst.append(lst_con)
+        allChar += lst
+        print('\n'.join(allChar))
+        time.sleep(1)
+print("Muziro")
+print("Keep Coding")
+print("HTML,CSS,JAVASCRIPT,PHP,JAVA,KOTLIN")
+import time
+
+print('\n')
+love = ['Muamar', 'Zidan', '2020']
+print(' '.join(love))
+print('\n')
+
+print('Hold On,Geetting Error :)')
+print('\n')
+time.sleep(2)
+print('Muziro...')
+sentence = "AlwaysCoding"
+for char in sentence.split():
+    allChar = []
+    for y in range(12, -12, -1):
+        lst = []
+        lst_con = ''
+        for x in range(-30, 30):
+            formula = ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3
+            if formula <= 0:
+                lst_con += char[(x) % len(char)]
+            else:
+                lst_con += ' '
+        lst.append(lst_con)
+        allChar += lst
+        print('\n'.join(allChar))
+        time.sleep(1)
+print("Muziro")
+print("Finally")
+print("HTML,CSS,JAVASCRIPT,PHP,JAVA,KOTLIN")
+import time
+
+print('\n')
+love = ['Muamar', 'Zidan', '2021']
+print(' '.join(love))
+print('\n')
+
+print('Hold On')
+print('\n')
+time.sleep(2)
+print('Muziro...And than')
+sentence = "And Succes"
+for char in sentence.split():
+    allChar = []
+    for y in range(12, -12, -1):
+        lst = []
+        lst_con = ''
+        for x in range(-30, 30):
+            formula = ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3
+            if formula <= 0:
+                lst_con += char[(x) % len(char)]
+            else:
+                lst_con += ' '
+        lst.append(lst_con)
+        allChar += lst
+        print('\n'.join(allChar))
+        time.sleep(1)
+print("Muziro")
+print("Web Developer")
+print("HTML,CSS,JAVASCRIPT,PHP,JAVA,KOTLIN")
